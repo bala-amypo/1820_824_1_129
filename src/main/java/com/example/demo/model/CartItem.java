a76e6cbb68e39c3f7cdbd70bbabe3ab05b57fecc
bala-amypo <bala@amypo.in>
2025-12-17 08:31:06 +0000

    User commit

--- src/main/java/com/example/demo/model/CartItem.java
+++ src/main/java/com/example/demo/model/CartItem.java
@@ -1,3 +1,37 @@
-public class CartItem{
-    
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Cart cart;
+
+    @ManyToOne(optional = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @PrePersist
+    @PreUpdate
+    public void validateQuantity() {
+        if (quantity == null || quantity <= 0) {
+            throw new RuntimeException("Quantity must be positive");
+        }
+    }
 }
