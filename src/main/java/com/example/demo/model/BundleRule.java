07ee040737e20566206cc8ad028eaf64e00c3800
bala-amypo <bala@amypo.in>
2025-12-18 09:11:56 +0000

    User commit

--- src/main/java/com/example/demo/model/BundleRule.java
+++ src/main/java/com/example/demo/model/BundleRule.java
@@ -1,17 +1,31 @@
 package com.example.demo.model;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
+@Entity
 @Getter
 @Setter
 public class BundleRule {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String ruleName;
+
+    @ElementCollection
     private List<Long> requiredProductIds;
+
     private Double discountPercentage;
+
     private Boolean active;
 }
