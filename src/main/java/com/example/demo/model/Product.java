61340e3cb2cd0aa5ec1b971874034add219c29b4
bala-amypo <bala@amypo.in>
2025-12-19 04:54:43 +0000

    User commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
+import jakarta.persistence.Column;
 @Entity
 public class Product {
 
