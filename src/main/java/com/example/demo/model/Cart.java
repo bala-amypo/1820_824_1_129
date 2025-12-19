60ff63860643cc687ea61d1d5976fa891d149777
bala-amypo <bala@amypo.in>
2025-12-19 04:21:23 +0000

    User commit

--- src/main/java/com/example/demo/model/Cart.java
+++ src/main/java/com/example/demo/model/Cart.java
@@ -5,12 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 @Entity
-@Getter
-@Setter
+
 public class Cart {
 
     @Id
