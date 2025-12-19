8ee0d7da92c2125146ed331f56dbc358f2ab9a92
bala-amypo <bala@amypo.in>
2025-12-19 04:49:10 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -1,1 +1,3 @@
-public class JwtA
+public class JwtAuthenticationFilter{
+    
+}
--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,27 +1,3 @@
-package com.example.demo.security;
-
-import org.springframework.stereotype.Component;
-
-import java.util.Base64;
-
-@Component
-public class JwtTokenProvider {
-
-    public String generateToken(String email) {
-        // simple fake token (Base64)
-        return Base64.getEncoder().encodeToString(email.getBytes());
-    }
-
-    public String getEmailFromToken(String token) {
-        return new String(Base64.getDecoder().decode(token));
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Base64.getDecoder().decode(token);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
+public class JwtTokenProvider{
+    
 }
