6f720b119fa47b353ce660b190026cdd4f3fb743
bala-amypo <bala@amypo.in>
2025-12-21 16:33:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/CartItemRepository.java
+++ src/main/java/com/example/demo/repository/CartItemRepository.java
@@ -1,13 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.CartItem;
-import com.example.demo.model.Cart;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
-    List<CartItem> findByCart(Cart cart);
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
+
+    List<CartItem> findByCartId(Long cartId);
 }
