dcca3ad1d0dcf75e702ddab796b7729f03660f4f
bala-amypo <bala@amypo.in>
2025-12-26 10:01:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/CartItemRepository.java
+++ src/main/java/com/example/demo/repository/CartItemRepository.java
@@ -2,6 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -11,5 +14,14 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
 
-    List<CartItem> findByCartIdAndMinQuantity(Long cartId, int minQuantity);
+    @Query("""
+           select ci
+           from CartItem ci
+           where ci.cart.id = :cartId
+             and ci.quantity >= :minQuantity
+           """)
+    List<CartItem> findByCartIdAndMinQuantity(
+            @Param("cartId") Long cartId,
+            @Param("minQuantity") int minQuantity
+    );
 }
