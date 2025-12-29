b9eecd3ce845f66cb747729c10098917baa6be2a
bala-amypo <bala@amypo.in>
2025-12-29 08:26:08 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -5,11 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtTokenProvider {
 
-    private final String secret = "dummy-secret";
-    private final long validityInMs = 3600000;
-
     public String generateToken(String email, String role, Long userId) {
-        // Stub implementation – tests mock this
         return "jwt-token";
     }
 
